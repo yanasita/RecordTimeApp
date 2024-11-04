e9f33028a5c6bb0a46c67bca9f578c3474ff6fa4
yanasita <9trin81999@gmail.com>
2024-11-04 12:30:49 +0900

    時間管理アプリの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const defaultTitle = "";
+const defaultTime = 0;
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(20)
+  const [records, setRecords] = useState([]);
+  const [titleText, setTitleText] = useState(defaultTitle);
+  const [timeNum, setTimeNum] = useState(defaultTime);
+  const [error, setError] = useState("");
+  const [totalTime, setTotalTime] = useState(defaultTime);
+
+  const addNewRecord = (textValue, numValue) =>{
+    const newRecord = {
+      title: textValue,
+      time: numValue
+    };
+
+    const updatedRecords = [...records, newRecord];
+    setRecords(updatedRecords);
+    
+    const sum = updatedRecords.reduce((accumulator, current) => accumulator + parseInt(current.time), 0);
+    setTotalTime(sum);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitleText(event.target.value)
+  }
+  const onChangeTime = (event) => {
+    setTimeNum(parseInt(event.target.value))
+  }
+
+  const onClickAddRecord = (textValue, numValue) =>{
+    if(textValue === "" || numValue <= 0) {
+      setError("入力されていない項目があります");
+      return;
+    }
+    setError("");
+    addNewRecord(textValue, numValue);
+    setTitleText(defaultTitle);
+    setTimeNum(defaultTime);
+  }
 
   return (
     <>
+      <h1>学習記録一覧</h1>
+
+      <div className="field">
+        <p>学習内容</p>
+        <input type="text" value={titleText} onChange={onChangeTitle}/>
+      </div>
+
+      <div className="field">
+        <p>学習時間</p>
+        <input type="number" value={timeNum} onChange={onChangeTime}/>
+        <p>時間</p>
+      </div>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>入力されている学習内容: {titleText}</p>
+        <p>入力されている学習時間: {timeNum}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <button onClick={() => {onClickAddRecord(titleText, timeNum)}}>登録</button>
+      <p style={{ color: 'red' }}>{error}</p>
+      <p>合計時間 {totalTime}/1000 (h)</p>
+      {records.map((record) => {
+        const { title, time } = record;
+        return <p key={title}>{title} {time}時間</p>
+      })}
     </>
   )
 }
+
+
 
 export default App
